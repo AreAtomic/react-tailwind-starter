3ba84bd5fbcf63fef45d2e21e6ab88613f70317f
AreAtomic <aureliensebe@gmail.com>
2022-03-15 09:53:54 +0100

    Init theme tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/animation-group.svg';
 
 function App() {
   return (
     <div className="text-center">
-      <header className="bg-gray-800 text-white flex flex-col items-center justify-center min-h-screen text-3xl">
-        <img src={logo} className="pointer-events-none h-40 animate-spin" alt="logo" />
+      <header className="bg-white-areatomic-500 flex flex-col text-purple-areatomic-500 items-center justify-center min-h-screen text-3xl">
+        <img src={logo} className="pointer-events-none h-full" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="text-blue-400"
+          className="text-blue-areatomic-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
