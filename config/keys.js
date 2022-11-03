24ed089af6c5349d4e51ae4d7b6cc439d262612c
tristanfrederick <fredericktristan@gmail.com>
2022-11-04 00:51:07 +0100

    modif importante

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:azerty01234root@areatomic.2iimiza.mongodb.net/test",
+    mongoURI: "mongodb+srv://admin:test1234@areatomic.2iimiza.mongodb.net/test",
     secretOrKey: "secret"
   };
